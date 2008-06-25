716a30931b5d1eb8cda0561d6e8efbe52555923d
gregoryjnewman <gregoryjnewman@4a6c792d-8048-0410-a9d2-3b35ac36266e>
2008-06-25 13:37:51 +0000

    Fixing tiny issues with tag_app

--- branches/nfa/projects/pinax/tag_app/urls.py
+++ branches/nfa/projects/pinax/tag_app/urls.py
@@ -5,6 +5,5 @@ from tag_app import views
 
 urlpatterns = patterns('',
     # all tags
-    #url(r'^$', 'tag_app.views.tags', name="tag_index"),
 	url(r'^(?P<tag>\w+)/$', 'tag_app.views.tags', name='tag_results'),
 )
